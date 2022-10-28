98be1039fabefc844179e374f50747af7467f6b0
Caroline Santos de Abreu <caroline@pop-os.localdomain>
2022-10-28 14:59:05 -0300

    done

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 let character = document.getElementById("character");
+let obstacle = document.getElementById("obstacle");
+let counter = 0;
 
 function moveLeft() {
   let left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
@@ -26,3 +28,20 @@ function keyDown(event) {
 }
 
 document.addEventListener("keydown", keyDown);
+
+obstacle.addEventListener("animationiteration", () => {
+  let random = Math.floor(Math.random() * 3);
+  left = random * 100;
+  obstacle.style.left = left + "px";
+  counter++;
+});
+
+setInterval(function () {
+  let characterLeft = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+  let obstacleLeft = parseInt(window.getComputedStyle(obstacle).getPropertyValue("left"));
+  let obstacleTop = parseInt(window.getComputedStyle(obstacle).getPropertyValue("top"));
+  if (characterLeft === obstacleLeft && obstacleTop < 500 && obstacleTop > 300) {
+    alert(`Game Over! Score: ${counter}`);
+    obstacle.style.animation = "none";
+  }
+}, 1);
